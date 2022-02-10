a39e8ea3224dff9b724c8ee910623f01600369f7
sompodsign <sompodsign@gmail.com>
2022-02-10 10:07:46 +0600

    quiz redux initiated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,11 @@ import HomeScreen from "./screens/HomeScreen";
 import JoinScreen from "./screens/JoinScreen";
 import LoginScreen from "./screens/LoginScreen";
 import './styles/base.css'
-import Words from "./components/words";
+import Words from "./screens/VocabularyScreen";
 import {ToastContainer} from "react-toastify";
 import {Container} from "react-bootstrap";
 import DictionaryScreen from "./screens/DictionaryScreen";
+import QuizScreen from "./screens/QuizScreen";
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
     <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/vocabulary" element={<Words />} />
+        <Route path="/vocabulary/quiz" element={<QuizScreen />} />
         <Route path="/dictionary" element={<DictionaryScreen />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/join" element={<JoinScreen />} />
--- frontend/src/screens/VocabularyScreen.js
+++ frontend/src/screens/VocabularyScreen.js
@@ -5,15 +5,15 @@ import { useNavigate } from 'react-router-dom';
 import '../styles/base.css'
 import { listWords } from '../redux/actions/wordActions';
 import {tabTitle} from "../utils/generalFunctions";
-import Table from "./table";
-import Input from "./input";
-import {PrimaryResBtn} from "./lightBtn";
+import Table from "../components/table";
+import Input from "../components/input";
+import {PrimaryResBtn} from "../components/lightBtn";
 
 function VocabularyScreen() {
 
   tabTitle('Vocabulary - VOCUP');
 
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState(null);
 
   const dispatch = useDispatch();
   const wordList = useSelector(state => state.wordList);
@@ -31,11 +31,11 @@ function VocabularyScreen() {
     }
 }, [dispatch, navigate, userInfo])
 
-  words = words.filter(word => word.word.toLowerCase().includes(inputValue.toLowerCase()));
+  words = inputValue !== null ? words.filter(word => word.word.toLowerCase().includes(inputValue.toLowerCase())) : words;
 
   return (
       <div>
-        <PrimaryResBtn onClick={() => navigate('/add-word')} title="Take a quiz?"/>
+        <PrimaryResBtn onClick={() => navigate('/vocabulary/quiz')} title="Take a quiz?"/>
         <Input  func={setInputValue} variant="outline-success" label="English Word"/>
             <Table words={words}/>
       </div>
