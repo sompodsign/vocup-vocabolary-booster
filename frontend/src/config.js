83b9dcc56a68f0698753d1d5ea7db74ab2ada802
sompodsign <sompodsign@gmail.com>
2022-02-16 21:41:43 +0000

    npm issue

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://20.212.104.33:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
