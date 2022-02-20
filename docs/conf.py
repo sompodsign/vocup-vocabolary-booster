3e20e19585f5b3bd15e234f6253e2066cc3bb16a
sompodsign <sompodsign@gmail.com>
2022-02-20 18:15:08 +0600

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,15 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
 import os
 import sys
 import django
@@ -14,7 +26,7 @@ django.setup()
 
 # -- Project information -----------------------------------------------------
 
-project = "Voccbolary Up"
+project = "Vocubolary Up"
 copyright = """2022, Shampad Sharkar"""
 author = "Shampad Sharkar"
 
