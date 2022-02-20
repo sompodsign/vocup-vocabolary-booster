ca05c8f8ae9c82e9c07177c7772edaead67132af
sompodsign <sompodsign@gmail.com>
2022-02-20 17:29:06 +0600

    new frontend build

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const serverApi = 'http://20.212.104.33:8000/api'
+const serverApi = 'shampad.live/api'
 const localApi = 'http://localhost:8000/api'
 
 const client = axios.create({
-    // baseURL: serverApi,
-    baseURL: localApi,
+    baseURL: serverApi,
+    // baseURL: localApi,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
