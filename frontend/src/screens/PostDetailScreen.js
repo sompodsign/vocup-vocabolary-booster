15ca52225193f86519be807c55eb89c91de839cb
sompodsign <sompodsign@gmail.com>
2022-03-06 01:40:55 +0600

    favicon added

--- frontend/src/screens/PostDetailScreen.js
+++ frontend/src/screens/PostDetailScreen.js
@@ -1,4 +1,3 @@
-import {MDBContainer} from "mdb-react-ui-kit";
 import "../styles/tutorialsStyle.css"
 
 import parse from "html-react-parser";
@@ -7,6 +6,7 @@ import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {retrieveTutorialAction} from "../redux/actions/tutorialActions";
 import {FillSpinner} from "../components/spinner";
+import {tabTitle} from "../utils/generalFunctions";
 
 export function PostDetail({match}) {
 
@@ -19,29 +19,30 @@ export function PostDetail({match}) {
     }, [dispatch, params.slug])
 
     const retrievePost = useSelector(state => state.retrieveTutorial)
-    const {loading, post, error} = retrievePost;
+    const {loading, post} = retrievePost;
 
+    tabTitle(post.title);
 
     return (
 
         <div className="">
 
-                <div>
-                    <div className="" className=""
-                         style={{background: "#14C07B"}}>
+            <div>
+                <div
+                    style={{background: "#14C07B"}}>
 
-                        <div className="-mt-4 p-10 container text-center">
-                            <h1 style={{color: "white", fontSize: "40px"}}><b>{loading ? "loading..." : post.title}</b></h1>
-                        </div>
-
+                    <div className="-mt-4 p-10 container text-center">
+                        <h1 style={{color: "white", fontSize: "40px"}}><b>{loading ? "loading..." : post.title}</b></h1>
                     </div>
 
-                    <div className="container mt-16">
-                        {loading && <div className="mx-auto text-center"><FillSpinner /></div>}
-                        {!loading && parse(String(post.body))}
-
-                    </div>
                 </div>
+
+                <div className="container mt-16">
+                    {loading && <div className="mx-auto text-center"><FillSpinner/></div>}
+                    {!loading && parse(String(post.body))}
+
+                </div>
+            </div>
 
         </div>
     )
--- frontend/src/screens/TutorialsScreen.js
+++ frontend/src/screens/TutorialsScreen.js
@@ -1,16 +1,17 @@
 import {Link, useNavigate} from 'react-router-dom';
 import {useSelector, useDispatch} from "react-redux";
-
-import {MDBContainer} from "mdb-react-ui-kit";
 import "../styles/tutorialsStyle.css"
 import {useEffect} from "react";
 import {listTutorials} from "../redux/actions/tutorialActions";
 import {getDaysAgo} from "../helpers/dateGeneralFunctions";
 import {Button} from "react-bootstrap";
 import {pluralize} from "../helpers/generalFunctions";
+import {tabTitle} from "../utils/generalFunctions";
 
 
 export default function TutorialScreen() {
+
+    tabTitle("Tutorials - VOCUP");
 
 
     const dispatch = useDispatch();
@@ -24,59 +25,59 @@ export default function TutorialScreen() {
     }, [dispatch])
 
 
-    return <div >
+    return <div>
 
-            <div
-                 style={{background: "#14C07B"}} className=" w-full -mt-4 p-10 -z-10 d-flex flex-column align-items-center">
+        <div
+            style={{background: "#14C07B"}} className=" w-full -mt-4 p-10 -z-10 d-flex flex-column align-items-center">
 
-                <h1  style={{color: "white", fontSize: "50px"}}><b>Tutorials</b></h1>
-                <p style={{color: "white", fontSize: "18px", fontWeight: "400"}}>Follow along with one
-                    of my {tutorials.length} development and sysadmin tutorials.</p>
-                <div className="container">
-                    {/*<i className="fas fa-search position-absolute left-3 top-6"/>*/}
-                    <input className="w-full h-16 p-2 border-1 rounded-2" placeholder="Search Tutorials"/>
-                </div>
+            <h1 style={{color: "white", fontSize: "50px"}}><b>Tutorials</b></h1>
+            <p style={{color: "white", fontSize: "18px", fontWeight: "400"}}>Follow along with one
+                of my {tutorials.length} development and sysadmin tutorials.</p>
+            <div className="container">
+                {/*<i className="fas fa-search position-absolute left-3 top-6"/>*/}
+                <input className="w-full h-16 p-2 border-1 rounded-2" placeholder="Search Tutorials"/>
+            </div>
+        </div>
+
+        <div className="mt-16 container">
+            {loading && <div>Loading...</div>}
+
+            <div className="flex justify-content-end lg:mb-0 mb-4">
+                <Link to="/create-tutorial">
+                    <Button variant="outline-success">Create Post</Button>{' '}
+                </Link>
             </div>
 
-            <div className="mt-16 container">
-                {loading && <div>Loading...</div>}
-
-                <div className="flex justify-content-end lg:mb-0 mb-4">
-                    <Link to="/create-tutorial">
-                        <Button variant="outline-success">Create Post</Button>{' '}
-                    </Link>
-                </div>
-
-                {
-                    tutorials && tutorials.map((tutorial) => {
-                        return <div className="" key={tutorial.id}>
-                            <Link to={tutorial.slug}>
-                                <a href="#">
-                                    <h1 class="post-header" style={{color:"black"}}>{tutorial.title}</h1>
-                                </a>
-                            </Link>
-                            <p class="short-desc">{tutorial.body.replace(/<[^>]*>?/gm, '').slice(0, 150) + "..."}</p>
-                            <div class="" className="flex">
-                                <p style={{
-                                    fontSize: "13px",
-                                    fontWeight: "500",
-                                    lineHeight: "1.6"
-                                }}>{pluralize(getDaysAgo(tutorial.created), "day")} ago</p>
-                                <p style={{
-                                    fontSize: "13px",
-                                    fontWeight: "500",
-                                    lineHeight: "1.6"
-                                }}>{Object.keys(tutorial.tags).length > 1 ?
-                                    tutorial.tags.map((tag) => {
-                                        return <span className="badge badge-pill badge-success ml-2">{tag}</span>
-                                    }) : <span className="badge badge-pill badge-success ml-2">{tutorial.tags}</span>
-                                }</p>
-                            </div>
+            {
+                tutorials && tutorials.map((tutorial) => {
+                    return <div className="" key={tutorial.id}>
+                        <Link to={tutorial.slug}>
+                            <a href="#">
+                                <h1 class="post-header" style={{color: "black"}}>{tutorial.title}</h1>
+                            </a>
+                        </Link>
+                        <p class="short-desc">{tutorial.body.replace(/<[^>]*>?/gm, '').slice(0, 150) + "..."}</p>
+                        <div class="" className="flex">
+                            <p style={{
+                                fontSize: "13px",
+                                fontWeight: "500",
+                                lineHeight: "1.6"
+                            }}>{pluralize(getDaysAgo(tutorial.created), "day")} ago</p>
+                            <p style={{
+                                fontSize: "13px",
+                                fontWeight: "500",
+                                lineHeight: "1.6"
+                            }}>{Object.keys(tutorial.tags).length > 1 ?
+                                tutorial.tags.map((tag) => {
+                                    return <span className="badge badge-pill badge-success ml-2">{tag}</span>
+                                }) : <span className="badge badge-pill badge-success ml-2">{tutorial.tags}</span>
+                            }</p>
                         </div>
-                    })
-                }
+                    </div>
+                })
+            }
 
-            </div>
+        </div>
 
         {/*</MDBContainer>*/}
     </div>
