23593b0350195c6d1192e88141ec81f3de1c6f68
sompodsign <sompodsign@gmail.com>
2022-03-13 02:06:27 +0600

    repeated date field added for task

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 user = get_user_model()
-print(user.objects.all())
+
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -18,10 +18,22 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+class TodoCategory(TimeStampedModel):
+    """
+    A model that represents a category for a todo item.
+    """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(user, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Todo(TimeStampedModel):
     """
     A model representing a todo item.
     """
+    category = models.ForeignKey(TodoCategory, on_delete=models.CASCADE, related_name='todos', blank=True, null=True)
     user = models.ForeignKey(user, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -29,6 +41,7 @@ class Todo(TimeStampedModel):
     reminder = models.DateTimeField(null=True, blank=True)
     repeat_every = models.IntegerField(default=0)
     repeat_until = models.DateTimeField(null=True, blank=True)
+    repeated_date = models.DateTimeField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if self.repeat_every > 0:
