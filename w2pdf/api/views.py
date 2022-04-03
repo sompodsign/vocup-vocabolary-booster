10afb1ff96b95cf8fc41ee3af94eb1d14a00b0a5
Shampad Sharkar <sompodsign@gmail.com>
2022-04-03 15:19:28 +0600

    minimal viewset created for word2pdf app

--- w2pdf/api/views.py
+++ w2pdf/api/views.py
@@ -1,3 +1,4 @@
+import subprocess
 
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
@@ -6,7 +7,15 @@ from w2pdf.api.serializers import WordFileSerializer
 from w2pdf.models import WordFile
 
 
+
 class WordFileViewSet(ModelViewSet):
     serializer_class = WordFileSerializer
     queryset = WordFile.objects.all()
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        tmp_file = request.data.get('file')
+        process = subprocess.Popen(['lowriter', '--convert-to', 'pdf', tmp_file.name, '--outdir', 'output_path'])
+        process.wait()
+        return super().create(request, *args, **kwargs)
+
