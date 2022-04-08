cb98050f4629dd6a73505b993e8edf2ae8f51877
Shampad Sharkar <sompodsign@gmail.com>
2022-04-09 00:40:06 +0600

    django app and api created for image compression.

---
+++ image_compress/api/views.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+from django.http import HttpResponse
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from PIL import Image
+import tempfile
+
+
+class ImageCompressView(APIView):
+    permissions_classes = (AllowAny,)
+
+    def post(self, request):
+        image = request.data.get('image') # already opened file in memory
+        quality = request.data.get('quality')
+        if not image or not quality:
+            return Response({'error': 'Image or quality not found'})
+        try:
+            quality = int(quality)
+        except ValueError:
+            return Response({'error': 'Quality must be an integer'})
+        if quality < 0 or quality > 100:
+            return Response({'error': 'Quality must be between 0 and 100'})
+        temp_file = tempfile.NamedTemporaryFile(suffix='.*')
+        image = Image.open(image)
+        image.save(temp_file, quality=quality, optimize=True)
+        temp_file.seek(0)
+        return HttpResponse(temp_file, content_type='image/*')
