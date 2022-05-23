281a5199ff25ed123bfb930fa8884bce71fb00e2
Shampad Sharkar <sompodsign@gmail.com>
2022-05-24 02:08:42 +0600

    model created for cheat meal

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -13,8 +13,14 @@ class TimeAndDateModel(models.Model):
         abstract = True
 
 
-class Expense(TimeAndDateModel):
+class DescriptionField(models.Model):
     description = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class Expense(TimeAndDateModel, DescriptionField):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
@@ -23,11 +29,19 @@ class Expense(TimeAndDateModel):
         return f'{self.description} - ৳{self.amount} - {self.date}'
 
 
-class Income(TimeAndDateModel):
-    description = models.CharField(max_length=255)
+class Income(TimeAndDateModel, DescriptionField):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.description} - ৳{self.amount} - {self.date}'
+
+
+class CheatMeal(TimeAndDateModel, DescriptionField):
+    date = models.DateField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.description} - {self.date}'
+
