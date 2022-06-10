9b451b6309acca6436c4989015bfa6f67d5be303
Shampad Sharkar <sompodsign@gmail.com>
2022-06-11 03:01:40 +0600

    email reader complete

--- counter/api/views.py
+++ counter/api/views.py
@@ -1,6 +1,6 @@
 import email
 import imaplib
-
+from bs4 import BeautifulSoup
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -12,11 +12,23 @@ from counter.models import Counter
 
 
 class CounterViewSet(ModelViewSet):
+    """
+    Create or update a counter based on name
+    * receives a name and a count through query param
+    * if name exists, update the count
+    * if name does not exist, create a new counter
+    """
     queryset = Counter.objects.all()
     serializer_class = CounterSerializer
     lookup_field = 'name'
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a counter based on name
+        * if name exists, return the count
+        * if name does not exist create a new counter
+        * if name exists but count is 0, return 0
+        """
         api_key = request.query_params.get('api-key')
         if api_key == "shampad":
             try:
@@ -32,29 +44,46 @@ class CounterViewSet(ModelViewSet):
 
 
 class GmailReader(APIView):
+    """
+    View to read emails from gmail account
+    * takes email and password in query params
+    * returns last 5 emails
+    """
     permission_classes = [AllowAny]
 
     def get(self, request):
-        # api_key = request.query_params.get('api-key')
-        # if api_key != "shampad":
-        mail = imaplib.IMAP4_SSL('imap.gmail.com')
-        mail.login("ss.unidev@gmail.com", "ahaobqwwwoyotshm")
+        api_key = request.query_params.get('api-key')
+        gmail_email = request.query_params.get('mail')
+        gmail_password = request.query_params.get('password')
+        if api_key == "shampad":
+            try:
+                mail = imaplib.IMAP4_SSL('imap.gmail.com')
+                mail.login(gmail_email, gmail_password)
+                mail.select("inbox")
+                result, data = mail.search(None, "ALL")
+                ids = data[0]
+                id_list = ids.split()
+                latest_email_id = id_list[-1]
+                result, data = mail.fetch(latest_email_id, "(RFC822)")
+                raw_email = data[0][1]
+                raw_email_string = raw_email.decode('utf-8')
+                email_message = email.message_from_string(raw_email_string)
 
-        try:
-            mail.select("inbox")
-            result, data = mail.search(None, "ALL")
-            ids = data[0]
-            id_list = ids.split()
-            latest_email_id = id_list[-1]
-            result, data = mail.fetch(latest_email_id, "(RFC822)")
-            raw_email = data[0][1]
-            raw_email_string = raw_email.decode('utf-8')
-            email_message = email.message_from_string(raw_email_string)
-            for part in email_message.walk():
-                if part.get_content_type() == "text/plain":
-                    body = part.get_payload(decode=True)
-                    print(body)
-        except Exception as ex:
-            print(ex)
-        # return Response({'success': False, 'message': 'Invalid api key'}, status=status.HTTP_404_NOT_FOUND)
+                plain_text_body = ''
+                if email_message.is_multipart():
+                    for part in email_message.walk():
+                        if part.get_content_type() == "text/plain":
+                            plain_text_body = part.get_payload(decode=True)
+                            break
+
+                if plain_text_body == '':
+                    plain_text_body = BeautifulSoup(email_message.as_string(), features="html.parser").get_text()
+
+                mail.close()
+                mail.logout()
+                return Response({'success': True, 'body': plain_text_body})
+            except Exception as ex:
+                print(ex)
+        else:
+            return Response({'success': False, 'message': 'Invalid api key'}, status=status.HTTP_404_NOT_FOUND)
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,8 +4,8 @@ const serverApi = 'https://shampad.live/api'
 const localApi = 'http://192.168.31.38/api'
 
 const client = axios.create({
-    // baseURL: serverApi,
-    baseURL: localApi,
+    baseURL: serverApi,
+    // baseURL: localApi,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
